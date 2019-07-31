3c65b1f3e9abf74e60f9429e144c07b96040f289
goandreus <andreshtml5@gmail.com>
2019-07-31 20:43:16 +0200

    insert route

---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div  class="about-page">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore reiciendis iusto nisi quae, doloribus molestias esse dignissimos necessitatibus delectus quas. Necessitatibus dicta numquam molestiae modi voluptatibus, reiciendis ea dignissimos sequi.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore reiciendis iusto nisi quae, doloribus molestias esse dignissimos necessitatibus delectus quas. Necessitatibus dicta numquam molestiae modi voluptatibus, reiciendis ea dignissimos sequi.</p>
+</div>
+</template>
+
+<style scoped>
+    .about-page{
+        text-align: center;
+        padding: 30px;
+    }
+</style>
